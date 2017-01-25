58380c1ebd7150f70e4d60dc740d15311063d68e
v-mxiaox <v-mxiaox@CNV-MXIAOX-W10>
2017-01-25 13:16:37 +0800

    Add project files.

---
+++ WebView_Test/WebView_Test/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebView_Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Browser.Source = "https://developer.xamarin.com/";
+           
+        }
+
+        void webOnNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            LoadingLabel.Text = "I am loading";
+        }
+
+        void webOnEndNavigating(object sender, WebNavigatedEventArgs e)
+        {
+            LoadingLabel.Text = "loading completed";
+        }
+
+
+        private void backClicked(object sender, EventArgs e)
+        {
+            // Check to see if there is anywhere to go back to
+            if (Browser.CanGoBack)
+            {
+                Browser.GoBack();
+            }
+            else
+            { // If not, leave the view
+                Navigation.PopAsync();
+            }
+        }
+
+        private void forwardClicked(object sender, EventArgs e)
+        {
+            if (Browser.CanGoForward)
+            {
+                Browser.GoForward();
+            }
+        }
+    }
+}
